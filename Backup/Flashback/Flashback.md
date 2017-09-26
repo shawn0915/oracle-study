d880e141eeb7117e158c8fb320079c09678225ae
shawn0915 <shawn2016@aliyun.com>
2017-09-26 16:49:36 +0800

    OCP--all-in-one--chapter19

--- Backup/Flashback/Flashback.md
+++ Backup/Flashback/Flashback.md
@@ -1,9 +1,10 @@
 # Flushback 闪回
 
-- Flashback Drop
-- Flashback Query
-- Flashback Data Archive
-- Flashback Database
+- [Flashback Drop](#flashback drop)
+- [Flashback Query](#flashback query)
+- [Flashback Database](#flashback database)
+- [Flashback Data Archive](#flashback data archive)
+
 
 [SQL-DEMO-FLASHBACK](../../sql_demo/backup/flashback.sql)
 
@@ -11,41 +12,62 @@
 
 不能对表截断而只能对表删除执行闪回操作。
 
+Flashback Drop命令仅使用于表，但是也会恢复所有相关的对象--外键约束除外。
+
+Flashback Drop不适用于存储在SYSTEM表空间中的表：通过上面介绍的查询不能找到这些表，因为直接将它们删掉并清除了。
+
 ## Flashback Query
 
 闪回查询的三种形式都依赖UNDO段的使用。
 
-## Flashback Data Archive
+所有形式的闪回查询依赖撤销数据来重构它在过去某个时间点的数据。
 
-FBDA, Flashback Data Archive进程
+可以查询以前某个时间点的数据库，但是永远不能对过去版本的数据执行DML。
 
-在介质损坏的情况下，如丢失数据文件，闪回技术无能为力。这种情况下必须借助于标准的备份、还原和恢复过程。
+### 闪回版本
 
+伪列
+
+关键字`versions between`
+
+闪回版本查询不能用于外部表、临时表或V$视图。原因是这些对象都不生成撤销数据。
+
+### 闪回事务
+
+`SELECT ANY TRANSACTION` 和 `FLASHBACK ANY TABLE` 权限必须授予将使用闪回事务功能的用户。
+
+可以在 `FLASHBACK_TRANSACTION_QUERY` 视图中查看事务，`UNDO_SQL` 列作为反转更改的一条语句的构成部分。
+
+通常由于约束违例造成的冲突可能使用闪回事务失败。
 
 ## Flashback Database
 
 不同于重做日志，不能多路复用和归档闪回日志，它们是自动创建和管理的。
 闪回数据库要求归档日志模式并使用`ALTER DATABASE OPEN RESETLOGS`来创建数据库的一个新化身。
 
-
-
-
-
-
-===================================================================================
-
-- 从回收站还原已删除的表
-
-- 使用闪回事务
-- 执行闪回表操作
-- 配置和监视 闪回数据库并执行闪回数据库操作
-
-- 闪回指定表
-
-
 闪回恢复区。闪回恢复区应大于数据库、最后一次完整备份以来所有增量备份以及尚未备份到磁带的所有归档重做日志文件的大小的总和。
 
 
-- 闪回版本
-- 闪回事务
+### 使用闪回数据库
+
+> 工具
+
+SQL*Plus/RMAN/Database Control
+
+> 方法
+
+- 关闭数据库
+- 加载数据库
+- 闪回到某个时间点、SCN或日志切换序列号
+- 使用`RESETLOGS`打开数据库
+
+
+## Flashback Data Archive
+
+闪回数据归档
+
+FBDA, Flashback Data Archive, 后台进程捕获行的前映像，将它们保存到归档中，并且清除已超过保留期限的数据的归档。
+
+在介质损坏的情况下，如丢失数据文件，闪回技术无能为力。这种情况下必须借助于标准的备份、还原和恢复过程。
+
 
--- sql_demo/backup/flashback.sql
+++ sql_demo/backup/flashback.sql
@@ -25,3 +25,228 @@ system@XE> alter system set db_recovery_file_dest = '/u01/app/oracle/flashback';
 system@XE> alter system set db_recovery_file_dest_size = 8G;
 
 系统已更改。
+
+-- 查看设置后的参数
+sys@XE> show parameter db_recovery;
+
+NAME				     TYPE	 VALUE
+------------------------------------ ----------- ------------------------------
+db_recovery_file_dest		     string	 /u01/app/oracle/flashback
+db_recovery_file_dest_size	     big integer 8G
+
+
+
+/*
+设置闪回保留目标时间
+参数 db_flashback_retention_target
+默认值1天，单位分钟
+ */
+system@XE> alter system set db_flashback_retention_target=240;
+
+系统已更改。
+
+sys@XE> show parameter flashback;
+
+NAME				     TYPE	 VALUE
+------------------------------------ ----------- ------------------------------
+db_flashback_retention_target	     integer	 240
+
+
+-- 干净地关闭并加载数据库
+sys@XE> shutdown immediate;
+数据库已经关闭。
+已经卸载数据库。
+ORACLE 例程已经关闭。
+
+sys@XE> startup mount;
+ORACLE 例程已经启动。
+
+Total System Global Area 1068937216 bytes
+Fixed Size		    2233344 bytes
+Variable Size		  650120192 bytes
+Database Buffers	  411041792 bytes
+Redo Buffers		    5541888 bytes
+数据库装载完毕。
+
+-- 启用闪回日志记录
+sys@XE> alter database flashback on;
+alter database flashback on
+*
+第 1 行出现错误:
+ORA-00439: 未启用功能: Flashback Database
+
+将启动RVWR进程并在SGA中分配闪回缓冲区。从现在起该进程启动将是自动的。
+
+
+-- 打开数据库
+alter database open;
+
+/*
+闪回监视
+ */
+sys@XE> select flashback_on from v$database;
+
+FLASHBACK_ON
+------------------
+NO
+
+已选择 1 行。
+
+SELECT *
+FROM V$FLASHBACK_DATABASE_LOG;
+SELECT *
+FROM V$FLASHBACK_DATABASE_STAT;
+select * from V$SGASTAT WHERE NAME = 'flashback generation buff';
+
+
+/*
+flashback drop
+ */
+hr@XE> create table t926(id int);
+
+表已创建。
+
+hr@XE> select * from tab;
+
+TNAME			       TABTYPE	CLUSTERID
+------------------------------ ------- ----------
+COUNTRIES		       TABLE
+DEPARTMENTS		       TABLE
+EMPLOYEES		       TABLE
+EMP_DETAILS_VIEW	       VIEW
+JOBS			       TABLE
+JOB_HISTORY		       TABLE
+LOCATIONS		       TABLE
+REGIONS 		       TABLE
+T926			       TABLE
+
+已选择9行。
+
+hr@XE> insert into t926 values(926);
+
+已创建 1 行。
+
+hr@XE> commit;
+
+提交完成。
+
+hr@XE> select * from t926;
+
+ID
+----------
+926
+
+已选择 1 行。
+
+hr@XE> drop table t926;
+
+表已删除。
+
+hr@XE> select * from t926;
+select * from t926
+              *
+第 1 行出现错误:
+ORA-00942: 表或视图不存在
+
+
+hr@XE> flashback table t926 to before drop;
+
+闪回完成。
+
+hr@XE> select * from t926;
+
+ID
+----------
+926
+
+已选择 1 行。
+
+hr@XE>
+
+/*
+回收站 recyclebin
+ */
+select * from dba_recyclebin;
+
+show recyclebin;
+
+/*
+Flashback Query
+ */
+EXECUTE dbms_flashback.enable_at_time();
+
+-- 冻结版本
+EXECUTE dbms_flashback.disable;
+
+-- 闪回表
+flashback table emp to timestamp to_timestamp('26-09-17 13:50:10','dd-mm-yy hh24:mi:ss');
+
+-- 闪回版本
+hr@XE>
+SELECT
+  id,
+  versions_xid,
+  versions_startscn,
+  versions_endscn,
+  versions_operation,
+  versions_starttime,
+  versions_endtime
+FROM t926 versions BETWEEN scn minvalue AND MAXVALUE;
+
+
+ID VERSIONS_XID     VERSIONS_STARTSCN VERSIONS_ENDSCN V
+---------- ---------------- ----------------- --------------- -
+VERSIONS_STARTTIME
+---------------------------------------------------------------------------
+VERSIONS_ENDTIME
+---------------------------------------------------------------------------
+926
+
+
+已选择 1 行。
+
+
+/*
+闪回数据归档
+ */
+-- 创建表空间
+CREATE TABLESPACE fda DATAFILE 'fda1.dbf' size 10m;
+
+-- 创建一个保留时间为7年的FDA
+CREATE flashback archive fla1 TABLESPACE fda retention 7 YEAR;
+
+-- 创建模式，并授予DBA角色
+GRANT dba TO fbdauser IDENTIFIED BY fbdauser;
+
+-- 授予用户操作归档的必要权限
+GRANT flashback archive ON flal TO fbdauser;
+
+-- 连接。创建一个表并为此表启用FDA
+[oracle@localhost XE]$ sqlplus fbdauser/fbdauser
+
+SQL*Plus: Release 11.2.0.2.0 Production on 星期二 9月 26 16:21:38 2017
+
+Copyright (c) 1982, 2011, Oracle.  All rights reserved.
+
+
+连接到:
+Oracle Database 11g Express Edition Release 11.2.0.2.0 - 64bit Production
+
+fbdauser@XE> create table t1 as select * from all_users;
+
+表已创建。
+
+fbdauser@XE> alter table t1 flashback archive fla1;
+alter table t1 flashback archive fla1
+*
+第 1 行出现错误:
+ORA-00439: 未启用功能: Flashback Data Archive
+
+
+fbdauser@XE>
+
+-- 删除表的除归档保护
+alter table fbdauser.t1 no flashback archive;
+
+-- 删除闪回数据归档
+drop flashback archive fla1;
